4cd4a6f405182dcf3c9eda4f30ef78713329dd6f
Nicnanun Ppongpattarawit <nichanun.lak@gmail.com>
2017-10-19 14:57:49 +0700

    no message

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'GP39z3zYbeJR4E7JP0lenQ0MIbtosME8GqpySYskc9kC08TnUGuHAEwjvLfFBFc2NPKgUZTqw7/a7OXcU978eSnpIm+/ymGen+grtyyfWbeSwWBCgar4NoRipq4g+kXTQjYOfX8ZixnAj2tapZc5ygdB04t89/1O/w1cDnyilFU=;
+$access_token = 'GP39z3zYbeJR4E7JP0lenQ0MIbtosME8GqpySYskc9kC08TnUGuHAEwjvLfFBFc2NPKgUZTqw7/a7OXcU978eSnpIm+/ymGen+grtyyfWbeSwWBCgar4NoRipq4g+kXTQjYOfX8ZixnAj2tapZc5ygdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
